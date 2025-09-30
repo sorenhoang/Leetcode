e1655c0336b21b3b7d5d1d4f5986d6f16dd61e76
soren <hoangducbinh.dev@gmail.com>
2025-09-30 08:51:30 +0700

    [SR-30-09-25] 2034 and 2221

---
+++ Solves/2xxx/22xx/2221.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int triangularSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = n - 2; i >= 0; i--)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
+            }
+        }
+        return nums[0];
+    }
+};
