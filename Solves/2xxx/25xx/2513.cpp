ac13ca139003e38df95d1fc7dbebd71c45a59cef
SR Dev <hoangducbinh.dev@gmail.com>
2025-10-21 17:26:36 +0700

    update

--- Solves/2xxx/25xx/2513.cpp
+++ Solves/2xxx/25xx/2513.cpp
@@ -7,10 +7,10 @@ public:
     int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2)
     {
         long long lcm = 1ll * divisor1 * divisor2 / __gcd(divisor1, divisor2);
-        int left = 0, right = 1e9;
+        long long left = 0, right = 2e9;
         while (right - left > 1)
         {
-            int mid = (left + right) / 2;
+            int mid = 1ll * (left + right) / 2;
             int commonCount = mid / lcm;
             int count1 = mid / divisor1 - commonCount;
             int count2 = mid / divisor2 - commonCount;
