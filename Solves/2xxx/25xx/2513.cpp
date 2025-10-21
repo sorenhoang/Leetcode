7fc34e24d0e42772a3d911885f2910590d7f7357
SR Dev <hoangducbinh.dev@gmail.com>
2025-10-21 17:24:34 +0700

    [SR-21-10-25] LeetCode: 2513

---
+++ Solves/2xxx/25xx/2513.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2)
+    {
+        long long lcm = 1ll * divisor1 * divisor2 / __gcd(divisor1, divisor2);
+        int left = 0, right = 1e9;
+        while (right - left > 1)
+        {
+            int mid = (left + right) / 2;
+            int commonCount = mid / lcm;
+            int count1 = mid / divisor1 - commonCount;
+            int count2 = mid / divisor2 - commonCount;
+            int cur = mid - (count1 + count2 + commonCount);
+            cur -= max(0, uniqueCnt1 - count2) + max(0, uniqueCnt2 - count1);
+            if (cur >= 0)
+                right = mid;
+            else
+                left = mid;
+        }
+        return right;
+    }
+};
+
+int main()
+{
+    // Input: divisor1 = 2, divisor2 = 7, uniqueCnt1 = 1, uniqueCnt2 = 3
+    Solution sol;
+    int divisor1 = 2, divisor2 = 4, uniqueCnt1 = 8, uniqueCnt2 = 2;
+    int result = sol.minimizeSet(divisor1, divisor2, uniqueCnt1, uniqueCnt2);
+    cout << result << endl;
+    return 0;
+}
