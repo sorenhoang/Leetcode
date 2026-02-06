d0935e90912f39ea26bbe0e077a94fc2d802415f
SR Dev <hoangducbinh.dev@gmail.com>
2026-02-06 17:18:31 +0700

    Blind 75 Add Solution class with characterReplacement method to solve string replacement problem

---
+++ Solves/0xxx/04xx/0424.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int characterReplacement(string s, int k)
+    {
+        int ans = 0;
+        int n = s.size();
+        vector<vector<int>> countChar(n + 1, vector<int>(26, 0));
+        s = ' ' + s;
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int c = 0; c < 26; ++c)
+            {
+                countChar[i][c] = countChar[i - 1][c];
+            }
+            countChar[i][s[i] - 'A']++;
+        }
+
+        int r = 1;
+        for (int i = 1; i <= n; ++i)
+        {
+            int c = s[i] - 'A';
+            while (r <= n)
+            {
+                int diff = (r - i + 1) - (countChar[r][c] - countChar[i - 1][c]);
+                if (diff <= k)
+                    r++;
+                else
+                    break;
+            }
+
+            ans = max(ans, r - i);
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    cout << Solution().characterReplacement("ABAB", 2) << endl;
+}
