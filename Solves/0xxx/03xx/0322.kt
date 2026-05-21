e8ad842067b06b9745a25e71b80f67fb7c6a89b7
spartan-binhhoang <hoangducbinh.dev@gmail.com>
2026-05-21 20:33:22 +0700

    Add Solution class with coinChange method to determine the minimum number of coins needed to make a given amount

---
+++ Solves/0xxx/03xx/0322.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        val dp = IntArray(amount + 1) { amount + 2 }
+        dp[0] = 0
+
+        for (i in 1..amount) {
+            for (coin in coins) {
+                if (i < coin) continue
+
+                dp[i] = minOf(dp[i], dp[i - coin] + 1)
+            }
+        }
+
+        return if (dp[amount] > amount) -1 else dp[amount]
+    }
+}
