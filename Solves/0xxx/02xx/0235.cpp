22095c5c2fffcfa8851e1e8410c4417068ff67f2
spartan-binhhoang <hoangducbinh.dev@gmail.com>
2026-05-30 19:12:39 +0700

    add 235

---
+++ Solves/0xxx/02xx/0235.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr) return nullptr;
+
+        int curr = root->val;
+
+        if(curr > p->val && curr > q->val)
+        {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+
+        if(curr < p->val && curr < q->val)
+        {
+            return lowestCommonAncestor(root->right, q, p);
+        }
+        
+        return root;
+    }
+};
