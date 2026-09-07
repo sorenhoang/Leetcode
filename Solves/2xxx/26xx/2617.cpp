4897be37f35c076261974637c61ec83041235357
sorenhoang <hoangducbinh.dev@gmail.com>
2026-09-07 11:41:22 +0700

    add problem 2617 - implement minimumVisitedCells function to find the minimum number of cells visited in a grid

---
+++ Solves/2xxx/26xx/2617.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void update(vector<int>& t, int index, int val) {
+        int n = t.size() / 2;
+        t[index + n] = val;
+        for (int i = index + n; i > 1; i /= 2) {
+            t[i / 2] = min(t[i], t[i ^ 1]);
+        }
+    }
+    int query(vector<int>& t, int l, int r) {
+        int n = t.size() / 2;
+        int res = INT_MAX;
+        for (l += n, r += n + 1; l < r; l /= 2, r /= 2) {
+            if (l & 1)
+                res = min(res, t[l++]);
+            if (r & 1)
+                res = min(res, t[--r]);
+        }
+        return res;
+    }
+
+    int minimumVisitedCells(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+
+        vector<vector<int>> rows(n, vector<int>(2 * m, INT_MAX));
+        vector<vector<int>> cols(m, vector<int>(2 * n, INT_MAX));
+
+        update(cols[m - 1], n - 1, 1), update(rows[n - 1], m - 1, 1);
+
+        for (int r = n - 1; r >= 0; --r) {
+            for (int c = m - 1; c >= 0; --c) {
+                if (r == n - 1 && c == m - 1)
+                    continue;
+
+                int move_down =
+                    query(rows[r], c + 1, min(m - 1, c + grid[r][c]));
+                int move_right =
+                    query(cols[c], r + 1, min(n - 1, r + grid[r][c]));
+
+                int mn = min(move_down, move_right);
+                if (mn != INT_MAX)
+                    ++mn;
+
+                update(cols[c], r, mn);
+                update(rows[r], c, mn);
+
+                if (!r && !c)
+                    return mn == INT_MAX ? -1 : mn;
+            }
+        }
+
+        return 1;
+    }
+};
